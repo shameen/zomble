210a7f6dca3a8881b5d378b41fab6a6e1bd1b0e2
Shameen Azeez <shameen.azeez@googlemail.com>
2013-12-04 09:38:49 +0000

    replaced tabs with spaces

--- zomble.c
+++ zomble.c
@@ -5,10 +5,10 @@
 
 int main(void)
 {
-	long i = 0;			                // loop counter
-	int left = 0;		                // speed of left wheel
-	int right = 0;		                // speed of right wheel
-	int waitlonger = 0;	                // if set to 1, will wait a bit longer in between each loop
+    long i = 0;                            // loop counter
+    int left = 0;                        // speed of left wheel
+    int right = 0;                        // speed of right wheel
+    int waitlonger = 0;                    // if set to 1, will wait a bit longer in between each loop
     
     //variables
     int isZombie = 0;                   // 0 for human, 1 for zombie
@@ -16,21 +16,21 @@ int main(void)
     int maxspeed_human = 200;           // max speed for humans  (default: 200)
     int maxspeed_zombiecharge = 800;    // speed that zombies charge at (default: 800)
     int zombieCurTurnSpeed = 5;         // speed that zombies turn to look around (default: 5)
-	
-	//setup
-	InitMotors();
-	InitProx();
-	LedClear();
-	//start with LEDs on
-	for(i=0;i<8;i++) {
+    
+    //setup
+    InitMotors();
+    InitProx();
+    LedClear();
+    //start with LEDs on
+    for(i=0;i<8;i++) {
         SetLed(i,1);
     }
-	SetStepsRight(1);
-	SetStepsLeft(1);
-	SetSpeedRight(100);
-	SetSpeedLeft(100);
-	
-	while (1) {
+    SetStepsRight(1);
+    SetStepsLeft(1);
+    SetSpeedRight(100);
+    SetSpeedLeft(100);
+    
+    while (1) {
     
         if (isZombie == 1) {
             
@@ -66,14 +66,14 @@ int main(void)
             }
         }
         
-		
-		/*wait*/for(i=0;i<40000;i++) {asm("nop");}
-		SetSpeedRight(right);
-		SetSpeedLeft(left);
-		
-		if (waitlonger == 1) {
-			/*wait*/for(i=0;i<100000;i++) {asm("nop");}
-			waitlonger = 0;
-		}
-	}
+        
+        /*wait*/for(i=0;i<40000;i++) {asm("nop");}
+        SetSpeedRight(right);
+        SetSpeedLeft(left);
+        
+        if (waitlonger == 1) {
+            /*wait*/for(i=0;i<100000;i++) {asm("nop");}
+            waitlonger = 0;
+        }
+    }
 }
