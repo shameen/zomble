05884d70cb682019adaa92c11cb35e283facb12b
Shameen Azeez <shameen.azeez@googlemail.com>
2013-12-07 19:53:37 +0000

    updated curious from "Curious_Steve-2013-12-06.zip"

--- curious.c
+++ curious.c
@@ -15,105 +15,79 @@ int curious()
 	SetStepsLeft(1);
 	int left = 0;
 	int right = 0;
-	int waitlonger = 0;
+//	int waitlonger = 0;
     int curious = 0;
 	while(1){
 		while (curious == 0) {
 			int j;
 			for(i=0;i<100000;i++) {asm("nop");}
 
-			left+=38;
-			right+=38;
+			left =200;
+			right = 200;
 			left = left * 0.9;
 			right = right * 0.9;
 
-			/*light up led if something is near it in that direction*/
-			/*and also adjusts wheel speed depending on which ones are lit up*/
-			for (j=0;j<7;j++) {
-				if (GetProx(j)>=500) {	
-					switch (j) {
-						case 0:left+=150;right+=100;break;
-						case 1:left+=100;right-=100;break;
-						case 2:left+=300;right-=400;break;
-						case 3:left+=400;right-=300;break;
-						case 4:left-=300;right+=400;break;
-						case 5:left-=400;right+=300;break;
-						case 6:left-=100;right+=100;break;
-						case 7:right+=150;left+=100;break;
-					}
-					SetLed(j,1);
-				}
-				else SetLed(j,0);
-			}
-
-			//back sensors
-			if (GetProx(3)>800 || GetProx(4)>800) {
-				//spin around in panic
-				left = 700;
-				right = -800;
-				for(i=0;i<100000;i++) {asm("nop");}
-				 
-			}
-			//front sensors
-			if (GetProx(0)>800 || GetProx(7)>800)  {
-               curious = 1;
-				
-			}	
-			
-			
 			/*wait*/for(i=0;i<40000;i++) {asm("nop");}
 			SetSpeedRight(right);
 			SetSpeedLeft(left);
 
-			
+			if(GetProx(0) >= 500 || GetProx(7) >= 500){
+				for (j=0; j <=10000; j++){
+					left= -200;
+					right= 200;
+					for(i=0;i<40000;i++) {asm("nop");}
+					SetSpeedRight(right);
+					SetSpeedLeft(left);
+					curious = 1;
+					if(j == 9999)
+						j=0;	
+					if( GetProx(2) >= 550 ){ 
+						j=10000;
+						return 0;
+					}
+				}
+			}
 		}
 
-	while(curious==1){
+/*	while(curious==1){
 				left =0;
 				right =0;
 			SetSpeedRight(right);
 			SetSpeedLeft(left);
 			    for(i=0;i<500000;i++) {asm("nop");}
-			
-					while(1){
-						if(GetProx(2)>800 && GetProx(3)>800 ){
-							left=400;
-							right=400;
-						}else if(GetProx(2)>800 ){
-							left=300;
-							right=300;
-
-					
-						
-						}else if(GetProx(3)>800 ){
-							left=350;
-							right=300;
-
-					
-						}	else if(GetProx(0)>800 || GetProx(7)>800   ){
-							left=-300;
-							right=400;
-						for(i=0;i<800000;i++) {asm("nop");}
-  }else if(GetProx(1)>800 || GetProx(0)>800   ){
-							left=200;
-							right=400;
-						
-						}
-						else  {
-						
-							left=500;
-							right=-400;
+				long intrestLevel = rand() % 2000000 + 2000000;
+intrestLevel = 200000000; 
+   			 	for(j=0;j<intrestLevel;j++){
+							if(GetProx(0)>800 && GetProx(7)>800 && !(GetProx(2)>800) ){
+								
+								for(i=0;i<800000;i++) {asm("nop");}
+  							}else if(GetProx(1)>800 && GetProx(0)>800   ){
+								left=200;
+								right=400;
+							}else if(GetProx(2)>800 && GetProx(3)>800 ){
+								left=400;
+								right=400;
+							}else if(GetProx(2)>800 ){
+								left=300;
+								right=320;
+							}else if(GetProx(3)>800 ){
+								left=350;
+								right=300;
+							}else{
+								left=400;
+								right=-400;
 							//for(i=0;i<500000;i++) {asm("nop");}
-						}
-                 /*wait*/for(i=0;i<40000;i++) {asm("nop");}
+							}
+                 intrestLevel = intrestLevel -1;
+                 //for(i=0;i<40000;i++) {asm("nop");}
 				SetSpeedRight(right);
-							SetSpeedLeft(left);
-		
+				SetSpeedLeft(left);
+		          
 
 			}
 				curious = 0;
 
-		}
+		}*/
 
 	}
 }
